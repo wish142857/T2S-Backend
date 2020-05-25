f62ba576bbdf96dd0d75d4c256e1f623828b4c86
wish142857 <wish142857@163.com>
2020-05-25 23:15:56 +0800

    update #6: update database in user and intention

--- intention/models.py
+++ intention/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from user.models import Teacher, Student
 
-# Create your models here.
+
+class Recruitment(models.Model):
+    RECRUITMENT_TYPE_CHOICES = [('UG', 'undergraduate'), ('MT', 'master'), ('DT', 'doctor')]
+    INTENTION_STATE_CHOICES = [('O', 'ongoing'), ('S', 'succeed'), ('F', 'fail')]
+
+    recruitment_id = models.AutoField(primary_key=True)                 # 招收意向 ID
+    publisher = models.ForeignKey(Teacher, on_delete=models.CASCADE)    # 发布者
+
+    recruitment_type = models.CharField(                                # 招收类型
+        max_length=2, choices=RECRUITMENT_TYPE_CHOICES, default='UG')
+    recruitment_number = models.IntegerField(null=False)                # 招收数量
+    research_fields = models.CharField(max_length=51200)                # 研究方向
+    introduction = models.CharField(max_length=51200)                   # 具体介绍
+    intention_state = models.CharField(                                 # 意向状态
+        max_length=1, choices=INTENTION_STATE_CHOICES, default='O')
+
+
+class Application(models.Model):
+    INTENTION_STATE_CHOICES = [('O', 'ongoing'), ('S', 'succeed'), ('F', 'fail')]
+
+    application_id = models.AutoField(primary_key=True)                 # 申请意向 ID
+    publisher = models.ForeignKey(Student, on_delete=models.CASCADE)    # 发布者
+
+    research_interests = models.CharField(max_length=51200)             # 兴趣方向
+    introduction = models.CharField(max_length=51200)                   # 个人简历
+    intention_state = models.CharField(                                 # 意向状态
+        max_length=1, choices=INTENTION_STATE_CHOICES, default='O')
--- user/models.py
+++ user/models.py
@@ -1,3 +1,58 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Teacher(models.Model):
+    GENDER_CHOICES = [('M', 'male'), ('F', 'female'), ('U', 'unknown')]
+    AUTH_STATE_CHOICES = [('UQ', 'unqualified'), ('QD', 'qualified')]
+
+    teacher_id = models.AutoField(primary_key=True)                                 # 导师 ID
+    user = models.ForeignKey(User, on_delete=models.CASCADE)                        # 用户账户
+
+    account = models.CharField(max_length=32, null=False, unique=True)              # 账号
+    password = models.CharField(max_length=32, null=False)                          # 密码
+    name = models.CharField(max_length=16, null=False)                              # 名称
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES, default='U')    # 性别
+    picture = models.ImageField()                                                   # 头像
+    signature = models.CharField(max_length=1024)                                   # 签名
+    school = models.CharField(max_length=32)                                        # 学校
+    department = models.CharField(max_length=32)                                    # 院系
+    title = models.CharField(max_length=32)                                         # 职务
+    introduction = models.CharField(max_length=51200)                               # 个人介绍
+    research_fields = models.CharField(max_length=51200)                            # 研究方向
+    research_achievements = models.CharField(max_length=51200)                      # 研究成果
+    phone = models.CharField(max_length=64)                                         # 电话
+    email = models.CharField(max_length=64)                                         # 邮箱
+    homepage = models.CharField(max_length=128)                                     # 主页
+    address = models.CharField(max_length=128)                                      # 地址
+    teacher_number = models.CharField(max_length=32)                                # 工号
+    id_number = models.CharField(max_length=32)                                     # 身份证号
+    auth_state = models.CharField(max_length=2, choices=AUTH_STATE_CHOICES, default='UQ')   # 认证状态
+
+
+class Student(models.Model):
+    GENDER_CHOICES = [('M', 'male'), ('F', 'female'), ('U', 'unknown')]
+    AUTH_STATE_CHOICES = [('UQ', 'unqualified'), ('QD', 'qualified')]
+
+    student_id = models.AutoField(primary_key=True)                                 # 学生 ID
+
+    account = models.CharField(max_length=32, null=False, unique=True)              # 账号
+    password = models.CharField(max_length=32, null=False)                          # 密码
+    name = models.CharField(max_length=16, null=False)                              # 名称
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES, default='U')    # 性别
+    picture = models.ImageField()                                                   # 头像
+    signature = models.CharField(max_length=1024)                                   # 签名
+    school = models.CharField(max_length=32)                                        # 学校
+    department = models.CharField(max_length=32)                                    # 院系
+    major = models.CharField(max_length=32)                                         # 专业
+    introduction = models.CharField(max_length=51200)                               # 个人介绍
+    research_experience = models.CharField(max_length=51200)                        # 研究经历
+    phone = models.CharField(max_length=64)                                         # 电话
+    email = models.CharField(max_length=64)                                         # 邮箱
+    homepage = models.CharField(max_length=128)                                     # 主页
+    address = models.CharField(max_length=128)                                      # 地址
+    student_number = models.CharField(max_length=32)                                # 学号
+    id_number = models.CharField(max_length=32)                                     # 身份证号
+    auth_state = models.CharField(max_length=2, choices=AUTH_STATE_CHOICES, default='UQ')   # 认证状态
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)                        # 用户账户
