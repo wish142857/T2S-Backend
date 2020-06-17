4491310d03f64caa6ed028899d993ef0fad98be3
wish142857 <wish142857@163.com>
2020-06-17 14:38:48 +0800

    update #41: modify follow

--- follow/views.py
+++ follow/views.py
@@ -1,4 +1,6 @@
 import json
+
+from django.contrib.auth.models import User
 from django.shortcuts import render
 from T2S_Backend.decorators import *
 from T2S_Backend.globals import *
@@ -11,47 +13,40 @@ from user.models import Teacher, Student
 def get_watchlist(request):
     # *** 请求处理 ***
     user = request.user
-    watchlist_teacher = []
-    watchlist_student = []
+    follow_list = None
     try:
-        teacher = Teacher.objects.get(user=user)
-        for u in teacher.follows.all():
-            try:
-                t = Teacher.objects.get(user=u)
-                watchlist_teacher.append(t.teacher_id)
-                continue
-            except Teacher.DoesNotExist:
-                pass
-            try:
-                s = Student.objects.get(user=u)
-                watchlist_student.append(s.student_id)
-                continue
-            except Student.DoesNotExist:
-                pass
-        response = {'status': True, 'info': S_QUERY_SUCCEED, 'watchlist_teacher': watchlist_teacher, 'watchlist_student': watchlist_student}
-        return HttpResponse(json.dumps(response, ensure_ascii=False))
+        follow_list = Teacher.objects.get(user=user).follows
     except Teacher.DoesNotExist:
         pass
     try:
-        student = Student.objects.get(user=user)
-        for u in student.follows.all():
-            try:
-                t = Teacher.objects.get(user=u)
-                watchlist_teacher.append(t.teacher_id)
-                continue
-            except Teacher.DoesNotExist:
-                pass
-            try:
-                s = Student.objects.get(user=u)
-                watchlist_student.append(s.student_id)
-                continue
-            except Student.DoesNotExist:
-                pass
-        response = {'status': True, 'info': S_QUERY_SUCCEED, 'watchlist_teacher': watchlist_teacher, 'watchlist_student': watchlist_student}
-        return HttpResponse(json.dumps(response, ensure_ascii=False))
+        follow_list = Student.objects.get(user=user).follows
     except Student.DoesNotExist:
         pass
-    response = {'status': False, 'info': F_ERROR_UNKNOWN_USER}
+    if follow_list is None:
+        response = {'status': False, 'info': F_ERROR_UNKNOWN_USER}
+        return HttpResponse(json.dumps(response, ensure_ascii=False))
+    watchlist_teachers = []
+    watchlist_students = []
+    for u in follow_list.all():
+        try:
+            t = Teacher.objects.get(user=u)
+            t_info = {'teacher_id': t.teacher_id, 'name': t.name, 'gender': t.gender,
+                      'school': t.school, 'department': t.department, 'auth_state': t.auth_state,
+                      'fans_number': t.user.teacher_fans.count() + t.user.student_fans.count(), 'is_followed': True}
+            watchlist_teachers.append(t_info)
+            continue
+        except Teacher.DoesNotExist:
+            pass
+        try:
+            s = Student.objects.get(user=u)
+            s_info = {'student_id': s.student_id, 'name': s.name, 'gender': s.gender,
+                      'school': s.school, 'department': s.department, 'auth_state': s.auth_state,
+                      'fans_number': s.user.teacher_fans.count() + s.user.student_fans.count(), 'is_followed': True}
+            watchlist_students.append(s_info)
+            continue
+        except Student.DoesNotExist:
+            pass
+    response = {'status': True, 'info': S_QUERY_SUCCEED, 'watchlist_teachers': watchlist_teachers, 'watchlist_students': watchlist_students}
     return HttpResponse(json.dumps(response, ensure_ascii=False))
 
 
@@ -60,9 +55,41 @@ def get_watchlist(request):
 def get_fanlist(request):
     # *** 请求处理 ***
     user = request.user
-    fanlist_teacher = [t.teacher_id for t in user.teacher_fans.all()]
-    fanlist_student = [s.student_id for s in user.student_fans.all()]
-    response = {'status': True, 'info': S_QUERY_SUCCEED, 'fanlist_teacher': fanlist_teacher, 'fanlist_student': fanlist_student}
+    follow_list = None
+    try:
+        follow_list = Teacher.objects.get(user=user).follows
+    except Teacher.DoesNotExist:
+        pass
+    try:
+        follow_list = Student.objects.get(user=user).follows
+    except Student.DoesNotExist:
+        pass
+    if follow_list is None:
+        response = {'status': False, 'info': F_ERROR_UNKNOWN_USER}
+        return HttpResponse(json.dumps(response, ensure_ascii=False))
+    fanlist_teachers = []
+    fanlist_students = []
+    for t in user.teacher_fans.all():
+        t_info = {'teacher_id': t.teacher_id, 'name': t.name, 'gender': t.gender,
+                  'school': t.school, 'department': t.department, 'auth_state': t.auth_state,
+                  'fans_number': t.user.teacher_fans.count() + t.user.student_fans.count(), 'is_followed': False}
+        try:
+            follow_list.get(username=t.user.username)
+            t_info['is_followed'] = True
+        except User.DoesNotExist:
+            pass
+        fanlist_teachers.append(t_info)
+    for s in user.student_fans.all():
+        s_info = {'student_id': s.student_id, 'name': s.name, 'gender': s.gender,
+                  'school': s.school, 'department': s.department, 'auth_state': s.auth_state,
+                  'fans_number': s.user.teacher_fans.count() + s.user.student_fans.count(), 'is_followed': False}
+        try:
+            follow_list.get(username=s.user.username)
+            s_info['is_followed'] = True
+        except User.DoesNotExist:
+            pass
+        fanlist_students.append(s_info)
+    response = {'status': True, 'info': S_QUERY_SUCCEED, 'fanlist_teachers': fanlist_teachers, 'fanlist_students': fanlist_students}
     return HttpResponse(json.dumps(response, ensure_ascii=False))
 
 
