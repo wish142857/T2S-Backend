2c40b7075fdaa6fdd7f33ea1029c5aba0eae0c5b
wish142857 <wish142857@163.com>
2020-06-27 23:12:56 +0800

    fix #74: fix a bug

--- T2S_Backend/utils.py
+++ T2S_Backend/utils.py
@@ -63,47 +63,47 @@ def auth_student(student_number, id_number) -> bool:
 def calculate_match_degree_t2s(t: Teacher, s: Student) -> int:
     ratio_1 = difflib.SequenceMatcher(None, t.department, s.department).quick_ratio()
     ratio_2 = difflib.SequenceMatcher(None, t.research_fields, s.research_interest).quick_ratio()
-    return ratio_1 + ratio_2
+    return (ratio_1 + ratio_2) * 1000
 
 
 # 计算匹配度  学生院系-导师院系  学生研究兴趣-导师研究方向
 def calculate_match_degree_s2t(s: Student, t: Teacher) -> int:
     ratio_1 = difflib.SequenceMatcher(None, s.department, t.department).quick_ratio()
     ratio_2 = difflib.SequenceMatcher(None, s.research_interest, t.research_fields).quick_ratio()
-    return ratio_1 + ratio_2
+    return (ratio_1 + ratio_2) * 1000
 
 
 # 计算匹配度  导师院系-导师院系  导师研究方向-导师研究方向
 def calculate_match_degree_t2t(t1: Teacher, t2: Teacher) -> int:
     ratio_1 = difflib.SequenceMatcher(None, t1.department, t2.department).quick_ratio()
     ratio_2 = difflib.SequenceMatcher(None, t1.research_fields, t2.research_fields).quick_ratio()
-    return ratio_1 + ratio_2
+    return (ratio_1 + ratio_2) * 1000
 
 
 # 计算匹配度  学生院系-学生院系  学生研究兴趣-学生研究兴趣
 def calculate_match_degree_s2s(s1: Student, s2: Student) -> int:
     ratio_1 = difflib.SequenceMatcher(None, s1.department, s2.department).quick_ratio()
     ratio_2 = difflib.SequenceMatcher(None, s1.research_interest, s2.research_interest).quick_ratio()
-    return ratio_1 + ratio_2
+    return (ratio_1 + ratio_2) * 1000
 
 
 # 计算匹配度  导师研究方向-招收意向研究方向
 def calculate_match_degree_t2r(t: Teacher, r: Recruitment) -> int:
-    return difflib.SequenceMatcher(None, t.research_fields, r.research_fields).quick_ratio()
+    return difflib.SequenceMatcher(None, t.research_fields, r.research_fields).quick_ratio() * 1000
 
 
 # 计算匹配度  学生学位-招收意向招收类型   学生研究兴趣-招收意向研究方向
 def calculate_match_degree_s2r(s: Student, r: Recruitment) -> int:
     ratio_1 = difflib.SequenceMatcher(None, s.degree, r.recruitment_type).quick_ratio()
     ratio_2 = difflib.SequenceMatcher(None, s.research_interest, r.research_fields).quick_ratio()
-    return (ratio_1 + ratio_2) / 2
+    return ((ratio_1 + ratio_2) / 2) * 1000
 
 
 # 计算匹配度  导师研究方向-申请意向兴趣方向
 def calculate_match_degree_t2a(t: Teacher, a: Application) -> int:
-    return difflib.SequenceMatcher(None, t.research_fields, a.research_interests).quick_ratio()
+    return difflib.SequenceMatcher(None, t.research_fields, a.research_interests).quick_ratio() * 1000
 
 
 # 计算匹配度  学生研究兴趣-申请意向兴趣方向
 def calculate_match_degree_s2a(s: Student, a: Application) -> int:
-    return difflib.SequenceMatcher(None, s.research_interest, a.research_interests).quick_ratio()
+    return difflib.SequenceMatcher(None, s.research_interest, a.research_interests).quick_ratio() * 1000
